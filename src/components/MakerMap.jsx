f85358de95c07684a8057af4a9d23a4f4c5e7879
AlexRodriguezZA <alekeyn1999@gmail.com>
2023-08-15 22:51:25 -0300

    first commit

--- src/components/MakerMap.jsx
+++ src/components/MakerMap.jsx
@@ -1,11 +1,15 @@
-//Funciones y hooks
+// Funciones y hooks
 import { Marker, useMap } from 'react-leaflet';
 import { useEffect } from "react";
+import L from 'leaflet'; 
+
+// Assets 
+import locationIcon from "../assets/images/icon-location.svg"
 
 const MakerMap = ({ dataIP }) => {
   const map = useMap();
 
-  //Desesctructuramos el dataIP y luego aplicamos la función de Flyto de leaflet
+  // Desestructuramos el dataIP y luego aplicamos la función de Flyto de leaflet
   useEffect(() => {
     if (dataIP) {
       const { lat, lng } = dataIP.location;
@@ -16,7 +20,16 @@ const MakerMap = ({ dataIP }) => {
     }
   }, [dataIP, map]);
 
-  return dataIP ? <Marker position={{ lat: dataIP.location.lat, lng: dataIP.location.lng }} /> : null;
+  // Define el ícono personalizado
+  const customIcon = L.icon({
+    iconUrl: locationIcon,
+    iconSize: [32, 32], // Ajusta el tamaño del ícono según sea necesario
+    iconAnchor: [16, 32], // Ajusta el anclaje del ícono si es necesario
+  });
+
+  return dataIP ? (
+    <Marker position={{ lat: dataIP.location.lat, lng: dataIP.location.lng }} icon={customIcon} />
+  ) : null;
 };
 
 export default MakerMap;
